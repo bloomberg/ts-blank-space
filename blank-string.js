560dc4ccf34beb4449a184ae8e7578dec70ba2f7
Ashley Claymore <aclaymore@bloomberg.net>
2023-11-24 15:44:49 +0000

    flatten blank-string

--- blank-string.js
+++ blank-string.js
@@ -1,13 +1,6 @@
 // Copyright 2023 Bloomberg Finance L.P.
 // Distributed under the terms of the Apache 2.0 license.
 // @ts-check
-/**
- * @param {[number, number]} a
- * @param {[number, number]} b
- */
-function sortHead(a, b) {
-    return a[0] - b[0];
-}
 
 /**
  * @param {string} input
@@ -22,7 +15,7 @@ function getSpace(input, start, end) {
 export default class BlankString {
     /** @type {string} */
     #input;
-    /** @type {[number, number][]} */
+    /** @type {number[]} */
     #ranges = [];
 
     /**
@@ -38,7 +31,7 @@ export default class BlankString {
      * @returns {void}
      */
     blank(start, end) {
-        this.#ranges.push([start, end]);
+        this.#ranges.push(start, end);
     }
 
     /**
@@ -51,29 +44,27 @@ export default class BlankString {
             return input;
         }
 
-        if (ranges.length === 1) {
-            const [start, end] = ranges[0];
+        if (ranges.length === 2) {
+            const [start, end] = ranges;
             return input.slice(0, start) +
                 getSpace(input, start, end) +
                 input.slice(end);
         }
 
-        ranges.sort(sortHead);
+        let previousStart = ranges[0];
+        let previousEnd = ranges[1];
+        let out = input.slice(0, previousStart);
+        out += getSpace(input, previousStart, previousEnd);
 
-        let previousRange = ranges[0];
-        let out = input.slice(0, previousRange[0]);
-        out += " ".repeat(previousRange[1] - previousRange[0]);
-
-        for (let i = 1; i < ranges.length; i++) {
-            const range = ranges[i];
-            if (previousRange[1] > range[0]) {
-                throw new Error(`overlapping ranges (${previousRange})+(${range})`);
-            }
-            out += input.slice(previousRange[1], range[0]);
-            out += getSpace(input, range[0], range[1]);
-            previousRange = range;
+        for (let i = 2; i < ranges.length; i += 2) {
+            const rangeStart = ranges[i];
+            const rangeEnd = ranges[i+1];
+            out += input.slice(previousEnd, rangeStart);
+            out += getSpace(input, rangeStart, rangeEnd);
+            previousStart = rangeStart;
+            previousEnd = rangeEnd;
         }
 
-        return out + input.slice(previousRange[1]);
+        return out + input.slice(previousEnd);
     }
 }
