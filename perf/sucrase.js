f0f0f6851c64c8770f3e1a6df7ba1b915e1ca63e
Ashley Claymore <aclaymore@bloomberg.net>
2023-11-24 13:40:55 +0000

    performance tests

---
+++ perf/sucrase.js
@@ -0,0 +1,30 @@
+// @ts-check
+import sucrase from "sucrase";
+import * as fs from "node:fs";
+
+function assert(v) {
+    if (!v) throw new Error();
+}
+
+const input = fs.readFileSync(process.argv[2], "utf-8");
+const count = Number(process.argv[3]) || 100;
+
+/** @type {sucrase.Options} */
+const options = {
+    transforms: ["typescript"],
+    disableESTransforms: true,
+    preserveDynamicImport: true,
+    filePath: "file.ts",
+    sourceMapOptions: {
+        compiledFilename: "file.ts"
+   },
+    production: true,
+    keepUnusedImports: true,
+};
+
+console.time("");
+for (let i = 0; i < count; i++) {
+    const output = sucrase.transform(input, options);
+    assert(output.code.length > 100);
+}
+console.timeEnd("");
---
+++ perf/this.js
@@ -0,0 +1,17 @@
+// @ts-check
+import * as fs from "node:fs";
+import blankSpace from "../index.js"
+
+function assert(v) {
+    if (!v) throw new Error();
+}
+
+const input = fs.readFileSync(process.argv[2], "utf-8");
+const count = Number(process.argv[3]) || 100;
+
+console.time("");
+for (let i = 0; i < count; i++) {
+    const output = blankSpace(input);
+    assert(output.length > 100);
+}
+console.timeEnd("");
