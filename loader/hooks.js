abc5f4ae37d268ad600fd6453f5c090638cfde14
Ashley Claymore <aclaymore@bloomberg.net>
2024-08-16 08:09:31 +0100

    feat(loader): support resolving .js to .ts

--- loader/hooks.js
+++ loader/hooks.js
@@ -2,6 +2,17 @@
 // Distributed under the terms of the Apache 2.0 license.
 import tsBlankSpace from "../out/index.js";
 
+export async function resolve(specifier, context, nextResolve) {
+    try {
+        return await nextResolve(specifier, context);
+    } catch (err) {
+        if (err.url?.endsWith(".js")) {
+            return nextResolve(err.url.slice(0, -".js".length) + ".ts");
+        }
+        throw err;
+    }
+}
+
 export async function load(url, context, nextLoad) {
     if (!url.endsWith(".ts")) {
         return nextLoad(url, context);
@@ -12,8 +23,8 @@ export async function load(url, context, nextLoad) {
     const transformedSource = tsBlankSpace(result.source.toString());
 
     return {
-      format,
-      shortCircuit: true,
-      source: transformedSource + "\n//# sourceURL=" + url,
+        format,
+        shortCircuit: true,
+        source: transformedSource + "\n//# sourceURL=" + url,
     };
 }
