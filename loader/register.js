34b9185a6f79a0d9b3861188c05cde291f38a9d9
Ashley Claymore <aclaymore@bloomberg.net>
2024-09-19 10:14:56 +0100

    fix: loader works on node 18.20
    Signed-off-by: Ashley Claymore <aclaymore@bloomberg.net>

--- loader/register.js
+++ loader/register.js
@@ -1,4 +1,3 @@
 import { register } from "node:module";
-import { pathToFileURL } from "node:url";
 
-register("./hooks.js", pathToFileURL(import.meta.filename));
+register("./hooks.js", import.meta.url);
